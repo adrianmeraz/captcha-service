02f05c3c18bcf4d8f1d1c9a4cd3365c0423bdcc4
Adrian <adrian@rydeas.com>
2024-08-18 13:19:06 -0500

    updated test

--- tests/lambdas/test_api_post_solve_captcha.py
+++ tests/lambdas/test_api_post_solve_captcha.py
@@ -14,24 +14,27 @@ RESOURCE_PATH = test_const.TEST_API_RESOURCE_PATH
 
 class ApiPostSolveCaptchaTests(BaseTestFixture):
     @respx.mock
+    @mock.patch.object(api_twocaptcha.TwoCaptchaAPI, 'get_environment')
     @mock.patch.object(api_twocaptcha.TwoCaptchaAPI, 'get_app_name')
-    @mock.patch.object(api_twocaptcha.TwoCaptchaAPI, 'get_domain_name')
+    @mock.patch.object(api_twocaptcha.TwoCaptchaAPI, 'get_base_domain_name')
     @mock.patch.object(api_twocaptcha.TwoCaptchaAPI, 'get_api_key')
     def test_ok(
         self,
         mocked_get_api_key,
         mocked_get_domain_name,
-        mocked_get_app_name
+        mocked_get_app_name,
+        mocked_get_environment
     ):
         mocked_get_api_key.return_value = 'IPSUMKEY'
         mocked_get_domain_name.return_value = 'ipsumlorem.com'
         mocked_get_app_name.return_value = 'big-service'
+        mocked_get_environment.return_value = 'dev'
 
         source = RESOURCE_PATH.joinpath('get_captcha_id.json')
         with as_file(source) as get_captcha_id_json:
             mocked_solve_captcha = self.create_ok_route(
                 method='POST',
-                url__eq='http://2captcha.com/in.php?key=IPSUMKEY&method=userrecaptcha&googlekey=6Le-wvkSVVABCPBMRTvw0Q4Muexq1bi0DJwx_mJ-&pageurl=https%3A%2F%2Fwww.example.com&json=1&proxy=&proxytype=MEGAPROXY.ROTATING.PROXYRACK.NET&pingback=big-service.ipsumlorem.com%2Fpingback-event',
+                url__eq='http://2captcha.com/in.php?key=IPSUMKEY&method=userrecaptcha&googlekey=6Le-wvkSVVABCPBMRTvw0Q4Muexq1bi0DJwx_mJ-&pageurl=https%3A%2F%2Fwww.example.com&json=1&proxy=&proxytype=MEGAPROXY.ROTATING.PROXYRACK.NET&pingback=https%3A%2F%2Fdev-big-service.ipsumlorem.com%2Fpingback-event',
                 _json=json.loads(get_captcha_id_json.read_text(encoding='utf-8'))
             )
 
@@ -61,3 +64,4 @@ class ApiPostSolveCaptchaTests(BaseTestFixture):
         self.assertEqual(mocked_get_api_key.call_count, 1)
         self.assertEqual(mocked_get_domain_name.call_count, 1)
         self.assertEqual(mocked_get_app_name.call_count, 1)
+        self.assertEqual(mocked_get_environment.call_count, 1)
