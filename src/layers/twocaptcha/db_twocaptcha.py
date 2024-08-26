81a3e150b6d96a0a1bbfb472e4d8859c81c101c5
Adrian <adrian@rydeas.com>
2024-08-25 23:17:22 -0500

    testing condition expression

--- src/layers/twocaptcha/db_twocaptcha.py
+++ src/layers/twocaptcha/db_twocaptcha.py
@@ -84,7 +84,8 @@ class UpdateCaptchaEvent(RecaptchaV2DB):
                 'PK': {'S': pk},
                 'SK': {'S': sk},
             },
-            update_expression=f'SET #est = :est, #mda = :mda, #cde = if_not_exists(#cde, :cde)',
+            update_expression=f'SET #est = :est, #mda = :mda, #cde = :cde',
+            condition_expression='attribute_not_exists(#cde)',
             expression_attribute_names={
                 '#est': 'EventStatus',
                 '#mda': 'ModifiedAt',
