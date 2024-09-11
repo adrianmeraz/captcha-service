7fb5dbd092040ffd64d7427d8cdad8d744a7c399
Adrian <adrian@rydeas.com>
2024-09-11 09:03:43 -0500

    added conditon

--- src/layers/twocaptcha/db_twocaptcha.py
+++ src/layers/twocaptcha/db_twocaptcha.py
@@ -1,10 +1,8 @@
-import typing
-
 from py_aws_core import decorators as aws_decorators, exceptions as aws_exceptions
 from py_aws_core.db_dynamo import ABCCommonAPI, DDBClient, UpdateItemResponse
-from src.layers.twocaptcha import entities
 
-from src.layers import const, logs, entities
+from src.layers import const, logs
+from src.layers.twocaptcha import entities
 
 logger = logs.logger
 __db_client = DDBClient()
@@ -23,46 +21,30 @@ class TwoCaptchaDB(ABCCommonAPI):
 
 
 class CreateCaptchaEvent(TwoCaptchaDB):
-    """
-        Updates Captcha Events
-    """
-
-    class Response(UpdateItemResponse):
-
-        @property
-        def captcha_event(self) -> entities.CaptchaEvent:
-            return entities.CaptchaEvent(self.attributes)
-
     @classmethod
     @aws_decorators.dynamodb_handler(client_err_map=aws_exceptions.ERR_CODE_MAP, cancellation_err_maps=[])
     def call(
         cls,
         db_client: DDBClient,
-        captcha_id: str,
-        status: const.EventStatus,
-        code: str = None,
-    ) -> Response:
-        pk = sk = cls.recaptcha_v2_event_create_key(captcha_id=captcha_id)
-        response = db_client.put_item(
-            key=cls.serialize_types({
-                'PK': pk,
-                'SK': sk,
-            }),
-            update_expression=f'SET #est = :est, #mda = :mda, #cde = :cde',
-            expression_attribute_names={
-                '#est': 'EventStatus',
-                '#mda': 'ModifiedAt',
-                '#cde': 'Code',
-            },
-            expression_attribute_values=cls.serialize_types({
-                ':est': status.value,
-                ':mda': cls.iso_8601_now_timestamp(),
-                ':cde': code,
-            }),
-            return_values='ALL_NEW'
+        _id: str,
+        code: str,
+        rate: str
+    ):
+        pk = sk = entities.TCWebhookEvent.create_key(_id=_id)
+        item = cls.get_put_item_map(
+            pk=pk,
+            sk=sk,
+            _type=entities.TCWebhookEvent.type(),
+            expire_in_seconds=None,
+            Code=code,
+            Rate=rate
         )
-        logger.info(f'{cls.__qualname__}#call, pk: {pk}, record updated')
-        return cls.Response(response)
+        response = db_client.put_item(
+            Item=item,
+            ConditionExpression='attribute_not_exists(pk)',
+        )
+        logger.info(f'{cls.__qualname__}#call -> response: {response}')
+        return response
 
 
 class CreateTCWebhookEvent(TwoCaptchaDB):
@@ -82,6 +64,7 @@ class CreateTCWebhookEvent(TwoCaptchaDB):
         )
         response = db_client.put_item(
             Item=item,
+            ConditionExpression='attribute_not_exists(pk)',
         )
         logger.debug(f'{cls.__qualname__}.call# -> response: {response}')
         return response
