3456bb046da11b97299a74066acd684700e639a6
Adrian <adrian@rydeas.com>
2024-09-11 11:05:54 -0500

    cleanup

--- src/layers/twocaptcha/db_twocaptcha.py
+++ src/layers/twocaptcha/db_twocaptcha.py
@@ -1,7 +1,7 @@
 from py_aws_core import decorators as aws_decorators, exceptions as aws_exceptions
-from py_aws_core.db_dynamo import ABCCommonAPI, DDBClient, UpdateItemResponse
+from py_aws_core.db_dynamo import ABCCommonAPI, DDBClient
 
-from src.layers import const, logs
+from src.layers import logs
 from src.layers.twocaptcha import entities
 
 logger = logs.logger
@@ -18,33 +18,6 @@ def get_db_client():
 
 class TwoCaptchaDB(ABCCommonAPI):
     pass
-
-
-class CreateCaptchaEvent(TwoCaptchaDB):
-    @classmethod
-    @aws_decorators.dynamodb_handler(client_err_map=aws_exceptions.ERR_CODE_MAP, cancellation_err_maps=[])
-    def call(
-        cls,
-        db_client: DDBClient,
-        _id: str,
-        code: str,
-        rate: str
-    ):
-        pk = sk = entities.TCWebhookEvent.create_key(_id=_id)
-        item = cls.get_put_item_map(
-            pk=pk,
-            sk=sk,
-            _type=entities.TCWebhookEvent.type(),
-            expire_in_seconds=None,
-            Code=code,
-            Rate=rate
-        )
-        response = db_client.put_item(
-            Item=item,
-            ConditionExpression='attribute_not_exists(pk)',
-        )
-        logger.info(f'{cls.__qualname__}#call -> response: {response}')
-        return response
 
 
 class CreateTCWebhookEvent(TwoCaptchaDB):
