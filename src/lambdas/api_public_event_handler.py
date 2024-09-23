47d584ebe8d316940834f4efb3e8b18dc9674569
Adrian <adrian@rydeas.com>
2024-09-23 10:51:26 -0500

    testing catch-all handler

---
+++ src/lambdas/api_public_event_handler.py
@@ -0,0 +1,25 @@
+from py_aws_core import decorators, utils as aws_utils
+
+from src.layers import events, exceptions, logs
+from src.layers.database import Database
+from src.layers.i_captcha import ICaptcha
+from src.layers.twocaptcha.captcha import TwoCaptcha
+
+logger = logs.logger
+
+
+@decorators.lambda_response_handler(raise_as=exceptions.CaptchaServiceException)
+def lambda_handler(raw_event, context):
+    logger.info(f'{__name__}, Incoming event: {raw_event}')
+    events.TwoCaptchaGetVerificationEvent(raw_event)
+    captcha_service = TwoCaptcha(database=Database())
+    response = process_event(captcha_service=captcha_service)
+    return aws_utils.build_lambda_response(
+        status_code=200,
+        body=response,
+        content_type='text/plain;charset=utf-8'
+    )
+
+
+def process_event(captcha_service: ICaptcha):
+    return captcha_service.get_verification_token()
