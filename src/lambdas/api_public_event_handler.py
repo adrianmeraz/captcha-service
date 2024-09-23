9e503477c47ca29bd6c80303cec166e64a5ba59b
Adrian <adrian@rydeas.com>
2024-09-23 17:12:42 -0500

    removed wrapped of event

--- src/lambdas/api_public_event_handler.py
+++ src/lambdas/api_public_event_handler.py
@@ -1,7 +1,6 @@
 from py_aws_core import decorators
 
 from src.layers import exceptions, logs
-from src.layers.events import HttpEvent
 from ._routes import apigw_router
 
 logger = logs.logger
@@ -10,11 +9,10 @@ logger = logs.logger
 @decorators.lambda_response_handler(raise_as=exceptions.CaptchaServiceException)
 def lambda_handler(event, context):
     logger.info(f'{__name__}, Incoming event: {event}')
-    event = HttpEvent(event)
     return route_event(event=event, context=context)
 
 
-def route_event(event: HttpEvent, context):
+def route_event(event, context):
     return apigw_router.handle_event(
         http_method=event.request_context.http_method,
         path=event.request_context.path,
