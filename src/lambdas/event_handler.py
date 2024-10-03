bbcba739c797d7e07d61dacb3d838dda6045194c
Adrian <adrian@rydeas.com>
2024-10-03 15:20:46 -0500

    cleanup

--- src/lambdas/event_handler.py
+++ src/lambdas/event_handler.py
@@ -1,14 +1,10 @@
 from py_aws_core import decorators
 
-from src.api_lambdas import import_all_package_modules
 from src.layers import exceptions, logs
 from src.layers.routing import get_router
 
 logger = logs.logger
 apigw_router = get_router()
-
-
-# import_all_package_modules('src.api_lambdas')
 
 
 @decorators.lambda_response_handler(raise_as=exceptions.CaptchaServiceException)
