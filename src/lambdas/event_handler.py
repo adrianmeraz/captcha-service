11eb79f8c49b49f9f339c54d6a21f61bab35029c
Adrian <adrian@rydeas.com>
2024-10-10 22:40:50 -0500

    updated workflow

--- src/lambdas/event_handler.py
+++ src/lambdas/event_handler.py
@@ -1,13 +1,11 @@
 from py_aws_core import decorators
 
-from src.layers import exceptions, logs
+from src.layers import exceptions
 from src.layers.routing import get_router
 
-logger = logs.get_logger()
 apigw_router = get_router()
 
 
 @decorators.lambda_response_handler(raise_as=exceptions.CaptchaServiceException)
 def lambda_handler(event, context):
-    logger.info('Incoming lambda event', aws_event=event)
     return apigw_router.handle_event(aws_event=event, aws_context=context)
